171ee6e262c4cbde8845a44ad547563306c90c58
Dilan <erdaldilan286@gmail.com>
2026-05-25 21:39:23 +0300

    Odemeler modulu CRUD tamamlandi

--- BEAUTY_PRO-/OdemeForm.Designer.cs
+++ BEAUTY_PRO-/OdemeForm.Designer.cs
@@ -28,12 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cmbMusteri = new System.Windows.Forms.ComboBox();
+            this.txtTutar = new System.Windows.Forms.TextBox();
+            this.cmbOdemeTipi = new System.Windows.Forms.ComboBox();
+            this.dtpOdemeTarihi = new System.Windows.Forms.DateTimePicker();
+            this.btnKaydet = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.dgvOdemeler = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOdemeler)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Müşteri";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tutar";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(78, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Ödeme Tipi";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Tarih";
+            // 
+            // cmbMusteri
+            // 
+            this.cmbMusteri.FormattingEnabled = true;
+            this.cmbMusteri.Location = new System.Drawing.Point(155, 26);
+            this.cmbMusteri.Name = "cmbMusteri";
+            this.cmbMusteri.Size = new System.Drawing.Size(121, 24);
+            this.cmbMusteri.TabIndex = 4;
+            // 
+            // txtTutar
+            // 
+            this.txtTutar.Location = new System.Drawing.Point(155, 64);
+            this.txtTutar.Name = "txtTutar";
+            this.txtTutar.Size = new System.Drawing.Size(100, 22);
+            this.txtTutar.TabIndex = 5;
+            // 
+            // cmbOdemeTipi
+            // 
+            this.cmbOdemeTipi.FormattingEnabled = true;
+            this.cmbOdemeTipi.Location = new System.Drawing.Point(155, 111);
+            this.cmbOdemeTipi.Name = "cmbOdemeTipi";
+            this.cmbOdemeTipi.Size = new System.Drawing.Size(121, 24);
+            this.cmbOdemeTipi.TabIndex = 6;
+            // 
+            // dtpOdemeTarihi
+            // 
+            this.dtpOdemeTarihi.Location = new System.Drawing.Point(155, 147);
+            this.dtpOdemeTarihi.Name = "dtpOdemeTarihi";
+            this.dtpOdemeTarihi.Size = new System.Drawing.Size(200, 22);
+            this.dtpOdemeTarihi.TabIndex = 7;
+            // 
+            // btnKaydet
+            // 
+            this.btnKaydet.Location = new System.Drawing.Point(22, 213);
+            this.btnKaydet.Name = "btnKaydet";
+            this.btnKaydet.Size = new System.Drawing.Size(75, 23);
+            this.btnKaydet.TabIndex = 8;
+            this.btnKaydet.Text = "Kaydet";
+            this.btnKaydet.UseVisualStyleBackColor = true;
+            this.btnKaydet.Click += new System.EventHandler(this.btnKaydet_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(155, 212);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(75, 23);
+            this.btnGuncelle.TabIndex = 9;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(296, 212);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(75, 23);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // dgvOdemeler
+            // 
+            this.dgvOdemeler.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvOdemeler.Location = new System.Drawing.Point(52, 288);
+            this.dgvOdemeler.Name = "dgvOdemeler";
+            this.dgvOdemeler.RowHeadersWidth = 51;
+            this.dgvOdemeler.RowTemplate.Height = 24;
+            this.dgvOdemeler.Size = new System.Drawing.Size(422, 150);
+            this.dgvOdemeler.TabIndex = 11;
+            // 
+            // OdemeForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvOdemeler);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnKaydet);
+            this.Controls.Add(this.dtpOdemeTarihi);
+            this.Controls.Add(this.cmbOdemeTipi);
+            this.Controls.Add(this.txtTutar);
+            this.Controls.Add(this.cmbMusteri);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "OdemeForm";
             this.Text = "OdemeForm";
+            this.Load += new System.EventHandler(this.OdemeForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvOdemeler)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cmbMusteri;
+        private System.Windows.Forms.TextBox txtTutar;
+        private System.Windows.Forms.ComboBox cmbOdemeTipi;
+        private System.Windows.Forms.DateTimePicker dtpOdemeTarihi;
+        private System.Windows.Forms.Button btnKaydet;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.DataGridView dgvOdemeler;
     }
 }
--- BEAUTY_PRO-/OdemeForm.cs
+++ BEAUTY_PRO-/OdemeForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,126 @@ namespace BEAUTY_PRO_
 {
     public partial class OdemeForm : Form
     {
+        DatabaseConnection db = new DatabaseConnection();
+
         public OdemeForm()
         {
             InitializeComponent();
+            dgvOdemeler.SelectionChanged += dgvOdemeler_SelectionChanged;
+        }
+        private void MusterileriGetir()
+        {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT MusteriID, AdSoyad FROM Musteriler",
+                db.GetConnection()
+            );
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            cmbMusteri.DataSource = dt;
+            cmbMusteri.DisplayMember = "AdSoyad";
+            cmbMusteri.ValueMember = "MusteriID";
+        }
+        private void OdemeleriListele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter(
+                "SELECT OdemeID, RandevuID, Tutar, OdemeTipi, OdemeTarihi FROM Odemeler",
+                db.GetConnection()
+            );
+
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            dgvOdemeler.DataSource = dt;
+        }
+        private void OdemeForm_Load(object sender, EventArgs e)
+        {
+            OdemeleriListele();
+            MusterileriGetir();
+
+            cmbOdemeTipi.Items.Add("Nakit");
+            cmbOdemeTipi.Items.Add("Kart");
+            cmbOdemeTipi.Items.Add("Havale");
+
+
+        }
+
+        private void btnKaydet_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand(
+    "INSERT INTO Odemeler(RandevuID, Tutar, OdemeTipi, OdemeTarihi) VALUES(@RandevuID, @Tutar, @OdemeTipi, @OdemeTarihi)",
+    db.GetConnection()
+);
+
+            komut.Parameters.AddWithValue("@RandevuID", 3);
+            komut.Parameters.AddWithValue("@Tutar", Convert.ToDecimal(txtTutar.Text));
+            komut.Parameters.AddWithValue("@OdemeTipi", cmbOdemeTipi.Text);
+            komut.Parameters.AddWithValue("@OdemeTarihi", dtpOdemeTarihi.Value.Date);
+
+            db.GetConnection().Open();
+            komut.ExecuteNonQuery();
+            db.GetConnection().Close();
+
+            OdemeleriListele();
+
+            MessageBox.Show("Ödeme başarıyla kaydedildi.");
+        }
+        private void dgvOdemeler_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgvOdemeler.CurrentRow != null)
+            {
+                txtTutar.Text = dgvOdemeler.CurrentRow.Cells["Tutar"].Value.ToString();
+                cmbOdemeTipi.Text = dgvOdemeler.CurrentRow.Cells["OdemeTipi"].Value.ToString();
+                dtpOdemeTarihi.Value = Convert.ToDateTime(dgvOdemeler.CurrentRow.Cells["OdemeTarihi"].Value);
+            }
+        }
+
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand(
+    "UPDATE Odemeler SET Tutar=@Tutar, OdemeTipi=@OdemeTipi, OdemeTarihi=@OdemeTarihi WHERE OdemeID=@OdemeID",
+    db.GetConnection()
+);
+
+            komut.Parameters.AddWithValue("@Tutar", Convert.ToDecimal(txtTutar.Text));
+            komut.Parameters.AddWithValue("@OdemeTipi", cmbOdemeTipi.Text);
+            komut.Parameters.AddWithValue("@OdemeTarihi", dtpOdemeTarihi.Value.Date);
+
+            komut.Parameters.AddWithValue(
+                "@OdemeID",
+                dgvOdemeler.CurrentRow.Cells["OdemeID"].Value
+            );
+
+            db.GetConnection().Open();
+            komut.ExecuteNonQuery();
+            db.GetConnection().Close();
+
+            OdemeleriListele();
+
+            MessageBox.Show("Ödeme güncellendi.");
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            SqlCommand komut = new SqlCommand(
+    "DELETE FROM Odemeler WHERE OdemeID=@OdemeID",
+    db.GetConnection()
+);
+
+            komut.Parameters.AddWithValue(
+                "@OdemeID",
+                dgvOdemeler.CurrentRow.Cells["OdemeID"].Value
+            );
+
+            db.GetConnection().Open();
+            komut.ExecuteNonQuery();
+            db.GetConnection().Close();
+
+            OdemeleriListele();
+
+            MessageBox.Show("Ödeme silindi.");
         }
     }
 }
+   
