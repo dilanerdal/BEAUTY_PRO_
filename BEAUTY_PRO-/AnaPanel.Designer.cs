762c687678f9a5cbfca75a858e69004aa193d486
Dilan <erdaldilan286@gmail.com>
2026-05-28 11:52:46 +0300

    Dashboard sistemi gelistirildi

--- BEAUTY_PRO-/AnaPanel.Designer.cs
+++ BEAUTY_PRO-/AnaPanel.Designer.cs
@@ -32,10 +32,32 @@
             this.btnDashboard = new System.Windows.Forms.Button();
             this.btnMusteriler = new System.Windows.Forms.Button();
             this.btnRandevular = new System.Windows.Forms.Button();
+            this.lblLogo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnHizmetler = new System.Windows.Forms.Button();
+            this.btnPersoneller = new System.Windows.Forms.Button();
+            this.btnOdemeler = new System.Windows.Forms.Button();
+            this.panelIcerik = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblBugunkuRandevu = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.lblAktifMusteri = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.lblAylikKazanc = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panelIcerik.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lblLogo);
             this.panel1.Location = new System.Drawing.Point(53, 33);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(200, 100);
@@ -46,7 +68,7 @@
             this.btnDashboard.BackColor = System.Drawing.Color.HotPink;
             this.btnDashboard.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnDashboard.ForeColor = System.Drawing.Color.White;
-            this.btnDashboard.Location = new System.Drawing.Point(53, 168);
+            this.btnDashboard.Location = new System.Drawing.Point(53, 148);
             this.btnDashboard.Name = "btnDashboard";
             this.btnDashboard.Size = new System.Drawing.Size(180, 45);
             this.btnDashboard.TabIndex = 1;
@@ -58,27 +80,201 @@
             this.btnMusteriler.BackColor = System.Drawing.Color.Navy;
             this.btnMusteriler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnMusteriler.ForeColor = System.Drawing.Color.White;
-            this.btnMusteriler.Location = new System.Drawing.Point(53, 243);
+            this.btnMusteriler.Location = new System.Drawing.Point(53, 212);
             this.btnMusteriler.Name = "btnMusteriler";
             this.btnMusteriler.Size = new System.Drawing.Size(180, 45);
             this.btnMusteriler.TabIndex = 2;
             this.btnMusteriler.Text = "Müşteriler";
             this.btnMusteriler.UseVisualStyleBackColor = false;
+            this.btnMusteriler.Click += new System.EventHandler(this.btnMusteriler_Click);
             // 
             // btnRandevular
             // 
-            this.btnRandevular.Location = new System.Drawing.Point(86, 326);
+            this.btnRandevular.BackColor = System.Drawing.Color.Navy;
+            this.btnRandevular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRandevular.ForeColor = System.Drawing.Color.White;
+            this.btnRandevular.Location = new System.Drawing.Point(53, 263);
             this.btnRandevular.Name = "btnRandevular";
-            this.btnRandevular.Size = new System.Drawing.Size(75, 23);
+            this.btnRandevular.Size = new System.Drawing.Size(180, 45);
             this.btnRandevular.TabIndex = 3;
             this.btnRandevular.Text = "Randevular";
-            this.btnRandevular.UseVisualStyleBackColor = true;
+            this.btnRandevular.UseVisualStyleBackColor = false;
+            this.btnRandevular.Click += new System.EventHandler(this.btnRandevular_Click);
+            // 
+            // lblLogo
+            // 
+            this.lblLogo.AutoSize = true;
+            this.lblLogo.BackColor = System.Drawing.Color.Navy;
+            this.lblLogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblLogo.ForeColor = System.Drawing.Color.White;
+            this.lblLogo.Location = new System.Drawing.Point(3, 0);
+            this.lblLogo.Name = "lblLogo";
+            this.lblLogo.Size = new System.Drawing.Size(163, 36);
+            this.lblLogo.TabIndex = 4;
+            this.lblLogo.Text = "BeautyPro";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(2, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(164, 18);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Güzellik Salonu Sistemi";
+            // 
+            // btnHizmetler
+            // 
+            this.btnHizmetler.BackColor = System.Drawing.Color.Navy;
+            this.btnHizmetler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHizmetler.ForeColor = System.Drawing.Color.White;
+            this.btnHizmetler.Location = new System.Drawing.Point(53, 365);
+            this.btnHizmetler.Name = "btnHizmetler";
+            this.btnHizmetler.Size = new System.Drawing.Size(180, 45);
+            this.btnHizmetler.TabIndex = 4;
+            this.btnHizmetler.Text = "Hizmetler";
+            this.btnHizmetler.UseVisualStyleBackColor = false;
+            this.btnHizmetler.Click += new System.EventHandler(this.btnHizmetler_Click);
+            // 
+            // btnPersoneller
+            // 
+            this.btnPersoneller.BackColor = System.Drawing.Color.Navy;
+            this.btnPersoneller.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPersoneller.ForeColor = System.Drawing.Color.White;
+            this.btnPersoneller.Location = new System.Drawing.Point(53, 416);
+            this.btnPersoneller.Name = "btnPersoneller";
+            this.btnPersoneller.Size = new System.Drawing.Size(180, 45);
+            this.btnPersoneller.TabIndex = 5;
+            this.btnPersoneller.Text = "Personeller";
+            this.btnPersoneller.UseVisualStyleBackColor = false;
+            this.btnPersoneller.Click += new System.EventHandler(this.btnPersoneller_Click);
+            // 
+            // btnOdemeler
+            // 
+            this.btnOdemeler.BackColor = System.Drawing.Color.Navy;
+            this.btnOdemeler.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnOdemeler.ForeColor = System.Drawing.Color.White;
+            this.btnOdemeler.Location = new System.Drawing.Point(53, 314);
+            this.btnOdemeler.Name = "btnOdemeler";
+            this.btnOdemeler.Size = new System.Drawing.Size(180, 45);
+            this.btnOdemeler.TabIndex = 6;
+            this.btnOdemeler.Text = "Ödemeler";
+            this.btnOdemeler.UseVisualStyleBackColor = false;
+            this.btnOdemeler.Click += new System.EventHandler(this.btnOdemeler_Click);
+            // 
+            // panelIcerik
+            // 
+            this.panelIcerik.BackColor = System.Drawing.Color.White;
+            this.panelIcerik.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panelIcerik.Controls.Add(this.panel4);
+            this.panelIcerik.Controls.Add(this.panel3);
+            this.panelIcerik.Controls.Add(this.panel2);
+            this.panelIcerik.Location = new System.Drawing.Point(290, 33);
+            this.panelIcerik.Name = "panelIcerik";
+            this.panelIcerik.Size = new System.Drawing.Size(900, 500);
+            this.panelIcerik.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.HotPink;
+            this.panel2.Controls.Add(this.lblBugunkuRandevu);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Location = new System.Drawing.Point(35, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(3, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 46);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "12";
+            // 
+            // lblBugunkuRandevu
+            // 
+            this.lblBugunkuRandevu.AutoSize = true;
+            this.lblBugunkuRandevu.ForeColor = System.Drawing.Color.White;
+            this.lblBugunkuRandevu.Location = new System.Drawing.Point(8, 65);
+            this.lblBugunkuRandevu.Name = "lblBugunkuRandevu";
+            this.lblBugunkuRandevu.Size = new System.Drawing.Size(117, 16);
+            this.lblBugunkuRandevu.TabIndex = 1;
+            this.lblBugunkuRandevu.Text = "Bugünkü Randevu";
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.panel3.Controls.Add(this.lblAktifMusteri);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Location = new System.Drawing.Point(302, 44);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 1;
+            // 
+            // lblAktifMusteri
+            // 
+            this.lblAktifMusteri.AutoSize = true;
+            this.lblAktifMusteri.ForeColor = System.Drawing.Color.White;
+            this.lblAktifMusteri.Location = new System.Drawing.Point(8, 65);
+            this.lblAktifMusteri.Name = "lblAktifMusteri";
+            this.lblAktifMusteri.Size = new System.Drawing.Size(78, 16);
+            this.lblAktifMusteri.TabIndex = 1;
+            this.lblAktifMusteri.Text = "Aktif Müşteri";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(3, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(89, 46);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "256";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.DarkOrange;
+            this.panel4.Controls.Add(this.lblAylikKazanc);
+            this.panel4.Controls.Add(this.label7);
+            this.panel4.Location = new System.Drawing.Point(567, 44);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 2;
+            // 
+            // lblAylikKazanc
+            // 
+            this.lblAylikKazanc.AutoSize = true;
+            this.lblAylikKazanc.ForeColor = System.Drawing.Color.White;
+            this.lblAylikKazanc.Location = new System.Drawing.Point(3, 59);
+            this.lblAylikKazanc.Name = "lblAylikKazanc";
+            this.lblAylikKazanc.Size = new System.Drawing.Size(83, 16);
+            this.lblAylikKazanc.TabIndex = 1;
+            this.lblAylikKazanc.Text = "Aylık Kazanç";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(3, 19);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(170, 46);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "₺28.450";
             // 
             // AnaPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1298, 601);
+            this.Controls.Add(this.panelIcerik);
+            this.Controls.Add(this.btnOdemeler);
+            this.Controls.Add(this.btnPersoneller);
+            this.Controls.Add(this.btnHizmetler);
             this.Controls.Add(this.btnRandevular);
             this.Controls.Add(this.btnMusteriler);
             this.Controls.Add(this.btnDashboard);
@@ -86,6 +282,15 @@
             this.Name = "AnaPanel";
             this.Text = "AnaPanel";
             this.Load += new System.EventHandler(this.AnaPanel_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panelIcerik.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -96,5 +301,20 @@
         private System.Windows.Forms.Button btnDashboard;
         private System.Windows.Forms.Button btnMusteriler;
         private System.Windows.Forms.Button btnRandevular;
+        private System.Windows.Forms.Label lblLogo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnHizmetler;
+        private System.Windows.Forms.Button btnPersoneller;
+        private System.Windows.Forms.Button btnOdemeler;
+        private System.Windows.Forms.Panel panelIcerik;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblBugunkuRandevu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label lblAylikKazanc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblAktifMusteri;
+        private System.Windows.Forms.Label label5;
     }
 }
--- BEAUTY_PRO-/AnaPanel.cs
+++ BEAUTY_PRO-/AnaPanel.cs
@@ -16,6 +16,14 @@ namespace BEAUTY_PRO_
         {
             InitializeComponent();
             AnaPanelTasarim();
+            DashboardVerileriGetir();
+        }
+
+        private void DashboardVerileriGetir()
+        {
+            lblAktifMusteri.Text = "256";
+            lblBugunkuRandevu.Text = "12";
+            lblAylikKazanc.Text = "₺28.450";
         }
         private void AnaPanelTasarim()
         {
@@ -24,12 +32,48 @@ namespace BEAUTY_PRO_
             panelMenu.Dock = DockStyle.Left;
             panelMenu.Width = 220;
             panelMenu.BackColor = Color.FromArgb(20, 20, 40);
+            panelMenu.Dock = DockStyle.Left;
 
             this.Controls.Add(panelMenu);
         }
         private void AnaPanel_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnMusteriler_Click(object sender, EventArgs e)
+        {
+            MusterilerForm frm = new MusterilerForm();
+            frm.ShowDialog();
+        }
+
+        private void btnRandevular_Click(object sender, EventArgs e)
+        {
+            RandevuForm frm = new RandevuForm();
+            frm.ShowDialog();
+        }
+
+        private void btnOdemeler_Click(object sender, EventArgs e)
+        {
+            OdemeForm frm = new OdemeForm();
+            frm.ShowDialog();
+        }
+
+        private void btnHizmetler_Click(object sender, EventArgs e)
+        {
+            HizmetlerForm frm = new HizmetlerForm();
+            frm.ShowDialog();
+        }
+
+        private void btnPersoneller_Click(object sender, EventArgs e)
+        {
+            PersonelForm frm = new PersonelForm();
+            frm.ShowDialog();
         }
     }
 }
